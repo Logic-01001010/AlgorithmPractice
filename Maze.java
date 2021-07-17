0d7755ad333790553b27c1b370a22d18e66c5a9a
E4STR00T <dnwn306@naver.com>
2021-07-17 23:42:02 +0900

    commit

--- Maze.java
+++ Maze.java
@@ -38,8 +38,8 @@ public class Maze {
     }
 
     public static void printMaze(){
-        for(int i=0; i<N-1; i++){
-            for(int j=0; j<N-1; j++){
+        for(int i=0; i<N; i++){
+            for(int j=0; j<N; j++){
                 System.out.print(maze[i][j]);
             }
             System.out.println();
